623ebb4309efb9e4bfc025b93a14333ebaec2fa3
mandeepsangha <91365504+mandeepsangha@users.noreply.github.com>
2022-08-08 18:54:34 +0100

    using variables

---
+++ src/apollo.js
@@ -0,0 +1,46 @@
+const { GraphQLClient, gql } = require("graphql-request");
+
+async function main() {
+  const endpoint = "https://api.github.com/graphql";
+
+  const graphQLClient = new GraphQLClient(endpoint, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "bearer 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",
+    },
+  });
+
+  const query = gql`
+    query ($login: String!) {
+      user(login: $login) {
+        repositories(affiliations: OWNER, isFork: false, first: 10) {
+          nodes {
+            name
+            languages(first: 10, orderBy: { field: SIZE, direction: DESC }) {
+              edges {
+                size
+                node {
+                  color
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const variables = {
+    login: "mandeepsangha",
+  };
+
+  const variables2 = {
+    login: "hachi-ops",
+  };
+
+  const queryData = await graphQLClient.request(query, variables2);
+  console.log(JSON.stringify(queryData, undefined, 2));
+}
+
+main();
